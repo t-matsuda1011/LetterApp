442310e6e746115545880f94bad2136325a0a78e
takeru matsuda <matsuuuda1011@gmail.com>
2025-03-10 18:02:35 +0900

    テスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("Firebase、マジ神");
+  const [userTitle, setUserTitle] = useState("");
+
+  // ユーザー入力のハンドリング
+  function handleOnChangeUserTitle(newTitle: string) {
+    setUserTitle(newTitle);
+  }
+
+  // タイトルの変更
+  function handleOnSendTitle() {
+    setTitle(userTitle);
+    setUserTitle("");
+  }
+
   return (
     <>
       <div>
-        <h1>Hello WOrld!</h1>
+        タイトル争奪戦
+        <h1>{title}</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <input
+            type="text"
+            id="new-title"
+            name="new-title"
+            value={userTitle}
+            onChange={(e) => handleOnChangeUserTitle(e.target.value)}
+          />
+          <input
+            type="button"
+            id="change-title"
+            name="change-title"
+            value="書き換える"
+            minLength={50}
+            onClick={() => handleOnSendTitle()}
+          />
+        </div>
       </div>
     </>
   );
